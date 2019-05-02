70c7393a2aa2fae078bbb25181a7da293a86e04d
Alexey Ldokov <alexldokov@alexldokov-macbookpro.roam.corp.google.com>
2019-05-02 10:05:13 +0100

    Updates

---
+++ precache-manifest.d4a4f95ffa9bbda29c5e004db4d1dfa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "724f0bf682ac909d437e",
+    "url": "/contactmanager/static/js/main.3f8334bb.chunk.js"
+  },
+  {
+    "revision": "5fe0c84b1bacdc8b9248",
+    "url": "/contactmanager/static/js/2.a46c82bc.chunk.js"
+  },
+  {
+    "revision": "5fe0c84b1bacdc8b9248",
+    "url": "/contactmanager/static/css/2.e4197af1.chunk.css"
+  },
+  {
+    "revision": "612db7e75d9a9b5e32341c38d25d09b8",
+    "url": "/contactmanager/index.html"
+  }
+];
